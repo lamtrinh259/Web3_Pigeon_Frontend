63197fc8423c74fc528d298d1e4150b8fe79e12a
duguowei1000 <duguowei1000@gmail.com>
2022-11-25 20:21:50 +0700

    for spheron 5

--- helping_functions/GW_finex.js
+++ helping_functions/GW_finex.js
@@ -1,7 +1,7 @@
 // const express = require("express");
-require("dotenv").config();
+// require("dotenv").config();
 // const router = express.Router();
-const axios = require("axios");
+import axios from "axios";
 // var _ = require('lodash');
 
 const baseUrl = "https://api-pub.bitfinex.com/v2/";
@@ -12,7 +12,7 @@ const callData =async() => {
     try {
         const response = await axios.get(`${baseUrl}/${pathParams}?${queryParams}`);
         const finexData = response.data;
-        console.log("finexData",finexData)
+        console.log("finexData",finexData[1][7])
         // res.status(200).json(finexData)
         return finexData
     
